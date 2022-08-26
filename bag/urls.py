a51ed3418bf6486cd6d50c026b6ebfa617bcbfb3
Gilbert Ng'eno <ngenogilbert07@gmail.com>
2022-08-26 09:46:54 +0300

    add a new item feature

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from bag.views import index
+from bag.views import add_item, index
 
 urlpatterns = [
     path('', index, name='index'),
+    path('add-item', add_item, name='add-item'),
 ]
