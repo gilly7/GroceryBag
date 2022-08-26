404a7931bcabe0ddebeec7ab6665a194fa974a5a
Gilbert Ng'eno <ngenogilbert07@gmail.com>
2022-08-26 09:38:43 +0300

    add a new item

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Item
 
-# Create your views here.
 @login_required
-def index(request):
-    return render(request, "index.html")
+def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        quantity = request.POST.get("quantity")
+        status = request.POST.get("status")
+        date = request.POST.get("date")
+        if name and quantity and status and date:
+            new_item = Item(name=name, quantity=quantity,
+                            status=status, date=date, user=request.user)
+            new_item.save()
+            messages.success(request, 'Item added successfully!')
+            return redirect('index')
+        messages.error(request, "One or more field(s) is missing!")
+        return redirect('add-item')
+    return render(request, "add.html")
